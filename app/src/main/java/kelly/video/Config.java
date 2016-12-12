b29e49c962ab94a77c66e93b9fc8efc11929c2e1
GavinYU <gavinyu@connect.hku.hk>
2016-12-12 23:58:17 +0800

    CloudBasedAlbum
    AndroidCloudBasedAlbum

---
+++ app/src/main/java/kelly/video/Config.java
@@ -0,0 +1,9 @@
+package kelly.video;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://i.cs.hku.hk/~klwang/upload.php";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "afudir";
+}
